38f6474fd680bc35c7a98a322f4019aeb6b3d9fe
PhantomOz <faniogor@gmail.com>
2025-08-02 02:50:28 +0100

    This adds the useSendTx Hook

--- frontend/hooks/send-tx.tsx
+++ frontend/hooks/send-tx.tsx
@@ -1,6 +1,49 @@
 import { useAppKitProvider, useAppKitAccount } from "@reown/appkit/react";
 import { BrowserProvider, Contract, ethers, formatUnits } from "ethers";
+import { toast } from "sonner";
+import twapAbi from "@/lib/abi.json";
 
-export default function SendTx() {
+export default function useSendTx() {
+    const { address, caipAddress, isConnected } = useAppKitAccount();
+    const { walletProvider } = useAppKitProvider("eip155");
 
+    const sendTx = async (makerAsset: string, takerAsset: string, totalAmount: number, chunks: number, interval: number, slippageBips: number) => {
+        if (!isConnected) {
+            toast.error("Please connect your wallet");
+            return;
+        };
+
+        const ethersProvider = new BrowserProvider(walletProvider as any);
+        const signer = await ethersProvider.getSigner();
+        const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
+        // The Contract object
+        const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
+        const twapBalance = await twapContract.createTWAPOrder(
+            makerAsset,
+            takerAsset,
+            totalAmount,
+            chunks,
+            interval,
+            slippageBips
+        );
+
+        console.log(twapBalance);
+    }
+
+    const cancelOrder = async (orderId: string) => {
+        if (!isConnected) {
+            toast.error("Please connect your wallet");
+            return;
+        };
+
+        const ethersProvider = new BrowserProvider(walletProvider as any);
+        const signer = await ethersProvider.getSigner();
+        const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
+        // The Contract object
+        const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
+        const twapBalance = await twapContract.cancelOrder(orderId);
+
+    }
+
+    return { sendTx, cancelOrder };
 }
