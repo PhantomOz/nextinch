928c1a6fc780b90fb9dfb4090895a37c341ffbd0
PhantomOz <faniogor@gmail.com>
2025-08-02 02:57:41 +0100

    This adds getters to the useSendTx Hook

--- frontend/hooks/send-tx.tsx
+++ frontend/hooks/send-tx.tsx
@@ -4,7 +4,7 @@ import { toast } from "sonner";
 import twapAbi from "@/lib/abi.json";
 
 export default function useSendTx() {
-    const { address, caipAddress, isConnected } = useAppKitAccount();
+    const { address, isConnected } = useAppKitAccount();
     const { walletProvider } = useAppKitProvider("eip155");
 
     const sendTx = async (makerAsset: string, takerAsset: string, totalAmount: number, chunks: number, interval: number, slippageBips: number) => {
@@ -18,7 +18,7 @@ export default function useSendTx() {
         const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
         // The Contract object
         const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
-        const twapBalance = await twapContract.createTWAPOrder(
+        const twapCreateTx = await twapContract.createTWAPOrder(
             makerAsset,
             takerAsset,
             totalAmount,
@@ -27,7 +27,7 @@ export default function useSendTx() {
             slippageBips
         );
 
-        console.log(twapBalance);
+        console.log(twapCreateTx);
     }
 
     const cancelOrder = async (orderId: string) => {
@@ -41,9 +41,42 @@ export default function useSendTx() {
         const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
         // The Contract object
         const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
-        const twapBalance = await twapContract.cancelOrder(orderId);
+        const twapCancelTx = await twapContract.cancelOrder(orderId);
 
+        console.log(twapCancelTx);
     }
 
-    return { sendTx, cancelOrder };
+    const getUserOrders = async () => {
+        if (!isConnected) {
+            toast.error("Please connect your wallet");
+            return;
+        };
+
+        const ethersProvider = new BrowserProvider(walletProvider as any);
+        const signer = await ethersProvider.getSigner();
+        const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
+        // The Contract object
+        const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
+        const twapOrders = await twapContract.getUserOrders(address);
+
+        console.log(twapOrders);
+    }
+
+    const getOrderDetails = async (orderId: string) => {
+        if (!isConnected) {
+            toast.error("Please connect your wallet");
+            return;
+        };
+
+        const ethersProvider = new BrowserProvider(walletProvider as any);
+        const signer = await ethersProvider.getSigner();
+        const twapAddress = process.env.NEXT_PUBLIC_TWAP_ADDRESS;
+        // The Contract object
+        const twapContract = new Contract(twapAddress, twapAbi.twap, signer);
+        const twapOrders = await twapContract.getOrderDetails(orderId);
+
+        console.log(twapOrders);
+    }
+
+    return { sendTx, cancelOrder, getUserOrders, getOrderDetails };
 }
